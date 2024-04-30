293b6780502c1a73fb62778f82864568d23689e6
Juillard Thibault <thibault.juillard@ecole2600.com>
2024-04-30 22:30:59 +0200

    coucou nathan

--- maintest.c
+++ maintest.c
@@ -7,6 +7,7 @@ int main()
 	printf("ptr1 = %p\n", ptr1);
 	char *ptr2 = my_alloc(25);
 	printf("ptr2 = %p\n", ptr2);
+	printf("ptr2 - ptr1 = %ld\n", (size_t)ptr2 - (size_t)ptr1);
 	clean(ptr1);
 	clean(ptr2);
 	struct chunk *t = (struct chunk*)((size_t)ptr2 - sizeof(struct chunk));
