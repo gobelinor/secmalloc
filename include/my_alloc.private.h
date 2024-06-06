cd2de05e4ad4fbf83587ea170fb0b13202edd469
nathan2626 <nathanjourno@yahoo.fr>
2024-06-07 01:31:44 +0200

    Création de la function lookup qui permet de parcourir la pool de metadata pour y chercher un bloc à allouer

--- include/my_alloc.private.h
+++ include/my_alloc.private.h
@@ -2,6 +2,7 @@
 #define __MY_ALLOC_PRIVATE_H__
 #include <stddef.h>
 #include "my_alloc.h"
+extern size_t heap_size;
 
 enum chunk_type 
 {
@@ -22,6 +23,7 @@ struct chunk *get_free_chunk(size_t size);
 struct chunk *get_free_chunk_raw(size_t size);
 
 struct chunk *get_last_chunk_raw();
+struct chunk *lookup(size_t size);
 
 void clean(void *ptr);
 #endif // __MY_ALLOC_H__
